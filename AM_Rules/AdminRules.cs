075187640e23f7cf093c87c8afb483056ea1b52f
Erwin Esparto <espartoerwin15@gmail.com>
2023-05-06 22:43:29 +0800

    Initial commit

---
+++ AM_Rules/AdminRules.cs
@@ -0,0 +1,53 @@
+﻿using AM_Data;
+
+namespace AM_Rules
+{
+    public class AdminRules
+    {
+        public bool checkAdminAccount(string username, string password)
+        {
+            AdminData d = new AdminData();
+            List<string> adminEmailList = d.getAdminEmail();
+            int existingemail = adminEmailList.IndexOf(username);
+
+            List<string> adminPassword = d.getAdminPassword();
+            int existingpassword = adminPassword.IndexOf(password);
+
+            if (existingemail == -1 || existingpassword == -1)
+            {
+                return false;
+            }
+            else
+            {
+                bool checkingindex = checkIndex(username, password);
+                if (checkingindex == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public bool checkIndex(string username, string password)
+        {
+            AdminData d = new AdminData();
+
+            List<string> adminEmailList = d.getAdminEmail();
+            int emailIndex = adminEmailList.FindIndex(a => a.Contains(username));
+
+            List<string> adminPassword = d.getAdminPassword();
+            int passwordIndex = adminPassword.FindIndex(b => b.Contains(password));
+
+            if (emailIndex == passwordIndex)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AM_UI/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using AM_Rules;
+
+namespace AM_UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> options = new List<string>()
+            {"Welcome. Are you a...?", "[1]STUDENT", "[2]TEACHER", "[3]ADMIN", "[0]EXIT"};
+            List<string> forms = new List<string>()
+            {"\n[1]Login", "[2]Sign Up", "[3]Forgot Password"};
+
+
+            int status = ShowOptions();
+            if (status == 1)
+            {
+                int action = showForm();
+
+            }
+            else if (status == 2)
+            {
+                int action = showForm();
+
+            }
+            if (status == 3)
+            {
+                adminLogin();
+            }
+
+            int ShowOptions()
+            {
+                foreach (string option in options)
+                {
+                    Console.WriteLine(option);
+                }
+                Console.Write("Input: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                return num;
+            }
+
+            int showForm()
+            {
+                foreach (string form in forms)
+                {
+                    Console.WriteLine(form);
+                }
+                Console.Write("Input: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                return num;
+
+            }
+
+            void adminLogin()
+            {
+                AdminRules r = new AdminRules();
+                Console.Write("\nType your email: ");
+                string adminEmail = Console.ReadLine();
+                Console.Write("Type your password: ");
+                string adminPass = Console.ReadLine();
+                bool check = r.checkAdminAccount(adminEmail, adminPass);
+
+                if (check == true)
+                {
+                    Console.WriteLine("Successful Login.");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect credentials.");
+                }
+            }
+        }
+    }
+}
